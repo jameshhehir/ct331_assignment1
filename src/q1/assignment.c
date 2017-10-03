3826534c3a75c671283a2e91a2779fb809a5ce2c
James Hehir <j.hehir1@nuigalway.ie>
2017-10-03 15:14:30 +0100

    Question 1 done

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -2,5 +2,11 @@
 
 int main(int arg, char* argc[]){
   printf("Hello assignment1.\n");
+  printf("Size of an int : %lu \n", sizeof(int));
+  printf("Size of an int pointer : %lu \n", sizeof(int*));
+  printf("Size of an long : %lu \n", sizeof(long));
+  printf("Size of an double pointer : %lu \n", sizeof(double*));
+  printf("Size of an pointer to a char pointer : %lu \n", sizeof(char**));
+
   return 0;
 }
